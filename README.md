1f7724f72c7e096cd6f7d4cbdf11cb2eb37768e1
= <=>
2020-04-15 17:28:47 +0700

    ninja wifi

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a project use HTML, CSS, Bootstrap 4, follow a web online is ninjawwifi.vn
+04/2020
---
+++ js/script.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    // autoplay: true,
+    effect: "slide",
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    }
+})
